ceed7e3bb778127b66ff703b8076510d81ed04eb
Alexis Vincent <mail@alexisvincent.io>
2020-12-01 15:30:32 +0000

    Dont ask to open browser

--- client/.dir-locals.el
+++ client/.dir-locals.el
@@ -5,5 +5,6 @@
    (cider-clojure-cli-global-options . "-A:dev")
    (cider-shadow-cljs-global-options . "-A:dev")
    (cider-preferred-build-tool . "shadow-cljs")
+   (cider-offer-to-open-cljs-app-in-browser . nil)
    (cider-default-cljs-repl . shadow)
    (cider-shadow-default-options . "client")))
