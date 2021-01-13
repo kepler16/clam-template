208191a4bce9f685e2aeff2538bb22407945a69c
Alexis Vincent <mail@alexisvincent.io>
2021-01-13 14:54:57 +0000

    Try new vercel building

--- prepare.sh
+++
@@ -1,5 +0,0 @@
-amazon-linux-extras install java-openjdk11
-
-curl -O https://download.clojure.org/install/linux-install-1.10.1.763.sh
-chmod +x linux-install-1.10.1.763.sh
-./linux-install-1.10.1.763.sh
